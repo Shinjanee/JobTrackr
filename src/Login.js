5409c55d3b3918a80cc81fe5592a821421422166
shinjanee-gupta <111811190+shinjanee-gupta@users.noreply.github.com>
2023-04-17 20:19:58 -0700

    fixes

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './Login.css';
 import googleLogo from './assets/google_logo.png';
-import jobTrackrBg from './assets/jobTrackrBg.png';
 
 const Login = ({ login }) => {
     return (
-        <div className="login-container" style={{ backgroundImage: `url(${jobTrackrBg})` }}>
+        <div className="login-container">
             <h1>Welcome to JobTrackr</h1>
             <button onClick={() => login()} className="login-btn">
                 <img src={googleLogo} alt="Google logo" className="google-logo" />
