faea3740d07ce0d380548877ddf342737033f465
shinjanee-gupta <111811190+shinjanee-gupta@users.noreply.github.com>
2023-04-29 14:58:36 -0700

    add app tracking

--- src/MatchJD.js
+++ src/MatchJD.js
@@ -1,12 +1,83 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
 
 const MatchJD = () => {
+  const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+  const [tries, setTries] = useState(parseInt(localStorage.getItem('tries')) || 5);
+  const buttonRef = useRef();
+
+  useEffect(() => {
+    setInputText('');
+    updateButtonStatus();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tries', tries);
+    updateButtonStatus();
+  }, [tries]);
+
+  useEffect(() => {
+    updateButtonStatus();
+  }, [inputText]);
+
+  const updateButtonStatus = () => {
+    if (inputText === '' || inputText.length < 50 || tries <= 0) {
+      buttonRef.current.disabled = true;
+      buttonRef.current.style.backgroundColor = '#ddd';
+      buttonRef.current.style.color = '#999';
+    } else {
+      buttonRef.current.disabled = false;
+      buttonRef.current.style.backgroundColor = '#3f51b5';
+      buttonRef.current.style.color = '#fff';
+    }
+  };
+
+  const handleInputTextChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleRightArrowClick = async () => {
+    if (tries > 0) {
+      const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        prompt: inputText,
+        max_tokens: 1024,
+        n: 1,
+        stop: ['\n']
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer YOUR_API_KEY'
+        }
+      });
+      setOutputText(response.data.choices[0].text);
+      setTries(prevTries => prevTries - 1);
+    }
+  };
+  
   return (
-    <div>
+    <div key={tries}>
       <h2>Match JD</h2>
-      <p>Content for the Match JD tab goes here.</p>
+      <div style={{ display: 'flex', alignItems: 'stretch' }}>
+        <textarea
+          style={{ flex: 1, height: '400px', resize: 'none', overflowY: 'scroll' }}
+          value={inputText}
+          onChange={handleInputTextChange}
+        />
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '0 10px' }}>
+          <button onClick={handleRightArrowClick} style={{ fontSize: '32px', padding: '10px 20px', borderRadius: '10px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)', backgroundColor: '#3f51b5', color: 'white', border: 'none', cursor: 'pointer' }} ref={buttonRef} disabled={!inputText || inputText.length < 50 || tries === 0}>
+            &rarr;
+          </button>
+        </div>
+        <textarea
+          style={{ flex: 1, height: '400px', resize: 'none', overflowY: 'scroll' }}
+          value={outputText}
+          readOnly
+        />
+      </div>
+      <p>Attempts remaining: {tries}</p>
     </div>
   );
-};
+}
 
 export default MatchJD;
--- src/TrackApplications.js
+++ src/TrackApplications.js
@@ -1,10 +1,110 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const API_URL = 'http://127.0.0.1:5000'; 
 
 const TrackApplications = () => {
+  const [applications, setApplications] = useState([]);
+  const [formVisible, setFormVisible] = useState(false);
+  const [formData, setFormData] = useState({
+    companyName: '',
+    jobLink: '',
+    position: '',
+    status: 'applied',
+  });
+
+  useEffect(() => {
+    fetch(`${API_URL}/applications`)
+      .then((response) => response.json())
+      .then((data) => setApplications(data));
+  }, []);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitting form data:", formData); 
+    fetch(`${API_URL}/applications`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then(() => {
+      setFormData({
+        companyName: '',
+        jobLink: '',
+        position: '',
+        status: 'applied',
+      });
+      setFormVisible(false);
+      fetch(`${API_URL}/applications`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Updated applications data:", data); 
+          setApplications(data);
+        });
+    });
+  };  
+
+  const handleDelete = (_id) => {
+    fetch(`${API_URL}/applications/${_id}`, {
+      method: 'DELETE',
+    }).then(() => {
+      fetch(`${API_URL}/applications`)
+        .then((response) => response.json())
+        .then((data) => setApplications(data));
+    });
+  };  
+
   return (
     <div>
       <h2>Track Applications</h2>
-      <p>Content for the Track Applications tab goes here.</p>
+      <button onClick={() => setFormVisible(!formVisible)}>Add Application</button>
+      {formVisible && (
+        <form onSubmit={handleSubmit}>
+          <label>Company Name: </label>
+          <input
+            type="text"
+            name="companyName"
+            value={formData.companyName}
+            onChange={handleChange}
+            required
+          />
+          <label>Job Link: </label>
+          <input
+            type="url"
+            name="jobLink"
+            value={formData.jobLink}
+            onChange={handleChange}
+            required
+          />
+          <label>Position: </label>
+          <input
+            type="text"
+            name="position"
+            value={formData.position}
+            onChange={handleChange}
+            required
+          />
+          <label>Status: </label>
+          <select name="status" value={formData.status} onChange={handleChange}>
+            <option value="applied">Applied</option>
+            <option value="interview">Interview</option>
+            <option value="rejected">Rejected</option>
+          </select>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+      <ul>
+      {applications.map((application) => (
+        <li key={application._id}>
+          {application.companyName} - {application.position} - {application.status}
+          <button onClick={() => handleDelete(application._id)}>Delete</button>
+        </li>
+      ))}
+      </ul>
     </div>
   );
 };
